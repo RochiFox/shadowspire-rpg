997807f6bdf9772ac664edf233f517a340436599
Rochi <rochithefox@gmail.com>
2025-02-10 15:04:06 +0200

    Added: added player movement

--- Shadowspire RPG/Assets/Scripts/PlayerState.cs
+++ Shadowspire RPG/Assets/Scripts/PlayerState.cs
@@ -6,8 +6,11 @@ public class PlayerState
 {
     protected PlayerStateMachine stateMachine;
     protected Player player;
-    private string animBoolName;
 
+    protected Rigidbody2D rb;
+
+    protected float xInput;
+    private string animBoolName;
 
     public PlayerState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName)
     {
@@ -19,11 +22,13 @@ public class PlayerState
     public virtual void Enter()
     {
         player.anim.SetBool(animBoolName, true);
+        rb = player.rb;
     }
 
     public virtual void Update()
     {
-        Debug.Log("I'm in " + animBoolName);
+
+        xInput = Input.GetAxisRaw("Horizontal");
     }
 
     public virtual void Exit()
