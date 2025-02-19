f3dbc4fcc54355546ab532a2da364012e61ff2b9
Rochi <rochithefox@gmail.com>
2025-02-19 15:27:09 +0200

    Added: added player and skill managers

---
+++ Shadowspire RPG/Assets/Scripts/SkillManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillManager : MonoBehaviour
+{
+    public static SkillManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(instance.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+}
