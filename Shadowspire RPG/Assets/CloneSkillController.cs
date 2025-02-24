4ca55463d4a9087e0c6ca804fdea44b17ced68d6
Rochi <rochithefox@gmail.com>
2025-02-24 16:20:13 +0200

    Added: added clone's attack

--- Shadowspire RPG/Assets/CloneSkillController.cs
+++ Shadowspire RPG/Assets/CloneSkillController.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class CloneSkillController : MonoBehaviour
 {
     private SpriteRenderer sr;
+    private Animator anim;
 
     [SerializeField] private float colorLoosingSpeed;
     private float cloneTimer;
+    [SerializeField] private Transform attackCheck;
+    [SerializeField] private float attackCheckRadius = 0.8f;
+    private Transform closestEnemy;
 
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -21,13 +26,74 @@ public class CloneSkillController : MonoBehaviour
         if (cloneTimer < 0)
         {
             sr.color = new Color(1, 1, 1, sr.color.a - (Time.deltaTime * colorLoosingSpeed));
+
+            if (sr.color.a == 0)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
-    public void SetupClone(Transform newTransform, float cloneDuration)
+    public void SetupClone(Transform newTransform, float cloneDuration, bool canAttack)
     {
+        if (canAttack)
+        {
+            anim.SetInteger("AttackNumber", Random.Range(1, 3));
+        }
+
         transform.position = newTransform.position;
 
         cloneTimer = cloneDuration;
+
+        FaceClosestTarget();
+    }
+
+
+    private void AnimationTrigger()
+    {
+        cloneTimer = -0.1f;
+    }
+
+    private void AttackTrigger()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(attackCheck.position, attackCheckRadius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+            {
+                hit.GetComponent<Enemy>().Damage();
+            }
+        }
+    }
+
+    private void FaceClosestTarget()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 25);
+
+        float closestDistance = Mathf.Infinity;
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+            {
+                float distanceToEnemy = Vector2.Distance(transform.position, hit.transform.position);
+
+                if (distanceToEnemy < closestDistance)
+                {
+                    closestDistance = distanceToEnemy;
+
+                    closestEnemy = hit.transform;
+                }
+            }
+        }
+
+        if (closestEnemy != null)
+        {
+            if (transform.position.x > closestEnemy.position.x)
+            {
+                transform.Rotate(0, 180, 0);
+            }
+        }
     }
 }
