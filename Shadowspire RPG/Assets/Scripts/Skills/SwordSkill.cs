cf6b1b6b1965ebae012e6f3057bba292a679d163
Rochi <rochithefox@gmail.com>
2025-02-26 17:00:54 +0200

    Added: added dots aim direction

--- Shadowspire RPG/Assets/Scripts/Skills/SwordSkill.cs
+++ Shadowspire RPG/Assets/Scripts/Skills/SwordSkill.cs
@@ -6,8 +6,41 @@ public class SwordSkill : Skill
 {
     [Header("Skill info")]
     [SerializeField] private GameObject swordPrefab;
-    [SerializeField] private Vector2 launchDirection;
+    [SerializeField] private Vector2 launchForce;
     [SerializeField] private float swordGravity;
+
+    private Vector2 finalDirection;
+
+    [Header("Aim dots")]
+    [SerializeField] private int numberOfDots = 20;
+    [SerializeField] private float spaceBetweenDots = 0.07f;
+    [SerializeField] private GameObject dotPrefab;
+    [SerializeField] private Transform dotsParent;
+
+    private GameObject[] dots;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        GenerateDots();
+    }
+
+    protected override void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            finalDirection = new Vector2(AimDirection().normalized.x * launchForce.x, AimDirection().normalized.y * launchForce.y);
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            for (int i = 0; i < dots.Length; i++)
+            {
+                dots[i].transform.position = DotsPosition(i * spaceBetweenDots);
+            }
+        }
+    }
 
     public void CreateSword()
     {
@@ -15,6 +48,52 @@ public class SwordSkill : Skill
 
         SwordSkillController newSwordScript = newSword.GetComponent<SwordSkillController>();
 
-        newSwordScript.SetupSword(launchDirection, swordGravity);
+        newSwordScript.SetupSword(finalDirection, swordGravity);
+
+        DotsActive(false);
+    }
+
+    public Vector2 AimDirection()
+    {
+        Vector2 playerPosition = player.transform.position;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - playerPosition;
+
+        return direction;
+    }
+
+    public void DotsActive(bool isActive)
+    {
+        for (int i = 0; i < dots.Length; i++)
+        {
+            dots[i].SetActive(isActive);
+        }
+    }
+
+    private void GenerateDots()
+    {
+        dots = new GameObject[numberOfDots];
+
+        for (int i = 0; i < numberOfDots; i++)
+        {
+            dots[i] = Instantiate(dotPrefab, player.transform.position, Quaternion.identity, dotsParent);
+
+            dots[i].SetActive(false);
+        }
+    }
+
+    private Vector2 DotsPosition(float t)
+    {
+        Vector2 startPosition = player.transform.position;
+
+        Vector2 velocity = new Vector2(
+            AimDirection().normalized.x * launchForce.x,
+            AimDirection().normalized.y * launchForce.y
+        );
+
+        float x = startPosition.x + velocity.x * t;
+        float y = startPosition.y + velocity.y * t + 0.5f * Physics2D.gravity.y * swordGravity * (t * t);
+
+        return new Vector2(x, y);
     }
 }
