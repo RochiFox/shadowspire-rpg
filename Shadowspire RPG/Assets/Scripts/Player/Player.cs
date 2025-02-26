9412814460f81a8599add04176581e6be9a68541
Rochi <rochithefox@gmail.com>
2025-02-26 15:35:46 +0200

    Added: added aim/throw sword animation

--- Shadowspire RPG/Assets/Scripts/Player/Player.cs
+++ Shadowspire RPG/Assets/Scripts/Player/Player.cs
@@ -33,8 +33,10 @@ public class Player : Entity
     public PlayerWallJumpState wallJumpState { get; private set; }
 
     public PlayerPrimaryAttackState primaryAttackState { get; private set; }
-
     public PlayerCounterAttackState counterAttackState { get; private set; }
+
+    public PlayerAimSwordState aimSwordState { get; private set; }
+    public PlayerCatchSwordState catchSwordState { get; private set; }
     #endregion
 
     protected override void Awake()
@@ -55,6 +57,9 @@ public class Player : Entity
 
         primaryAttackState = new PlayerPrimaryAttackState(this, stateMachine, "Attack");
         counterAttackState = new PlayerCounterAttackState(this, stateMachine, "CounterAttack");
+
+        aimSwordState = new PlayerAimSwordState(this, stateMachine, "AimSword");
+        catchSwordState = new PlayerCatchSwordState(this, stateMachine, "CatchSword");
     }
 
     protected override void Start()
---
+++ Shadowspire RPG/Assets/Scripts/Skills/SwordSkill.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSkill : Skill
+{
+    [Header("Skill info")]
+    [SerializeField] private GameObject swordPrefab;
+    [SerializeField] private Vector2 launchDirection;
+    [SerializeField] private float swordGravity;
+
+
+}
