8daaef27c529b8c46c0f345d056c91f09d6cfcae
Rochi <rochithefox@gmail.com>
2025-03-03 14:15:58 +0200

    Added: added blackhole prototype

---
+++ Shadowspire RPG/Assets/Scripts/Skills/Skill Controllers/BlackholeSkillController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackholeSkillController : MonoBehaviour
+{
+    [SerializeField] private GameObject hotKeyPrefab;
+    [SerializeField] private List<KeyCode> keyCodeList;
+
+    public float maxSize;
+    public float growSpeed;
+    public bool canGrow;
+
+    private List<Transform> targets = new List<Transform>();
+
+    void Update()
+    {
+        if (canGrow)
+        {
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(maxSize, maxSize), growSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Enemy>() != null)
+        {
+            collision.GetComponent<Enemy>().FreezeTime(true);
+
+            CreateHotKey(collision);
+        }
+    }
+
+    private void CreateHotKey(Collider2D collision)
+    {
+        if (keyCodeList.Count <= 0)
+        {
+            Debug.LogWarning("Not enough hot keys in a key code list!");
+            return;
+        }
+
+        GameObject newHotKey = Instantiate(hotKeyPrefab, collision.transform.position + new Vector3(0, 2), Quaternion.identity);
+
+        KeyCode choosenKey = keyCodeList[Random.Range(0, keyCodeList.Count)];
+        keyCodeList.Remove(choosenKey);
+
+        BlackholeHotKeyController newHotKeyScript = newHotKey.GetComponent<BlackholeHotKeyController>();
+
+        newHotKeyScript.SetupHotKey(choosenKey, collision.transform, this);
+    }
+
+    public void AddEnemyToList(Transform _enemyTransform) => targets.Add(_enemyTransform);
+}
