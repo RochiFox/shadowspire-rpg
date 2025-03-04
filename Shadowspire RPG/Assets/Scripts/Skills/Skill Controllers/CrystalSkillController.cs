1308105115913d5b9f1b829dd87f1f02e37da7a0
Rochi <rochithefox@gmail.com>
2025-03-04 16:48:14 +0200

    Added: added explosive to crystal

--- Shadowspire RPG/Assets/Scripts/Skills/Skill Controllers/CrystalSkillController.cs
+++ Shadowspire RPG/Assets/Scripts/Skills/Skill Controllers/CrystalSkillController.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class CrystalSkillController : MonoBehaviour
 {
-    private float crystalExistTimer;
+    private Animator anim => GetComponent<Animator>();
+    private CircleCollider2D cd => GetComponent<CircleCollider2D>();
 
-    public void SetupCrystal(float _crystalDuration)
+    private float crystalExistTimer;
+    private bool canExplode;
+    private bool canMove;
+    private float moveSpeed;
+    private bool canGrow;
+    private float growSpeed = 5;
+
+    public void SetupCrystal(float _crystalDuration, bool _canExplode, bool _canMove, float _moveSpeed)
     {
         crystalExistTimer = _crystalDuration;
+        canExplode = _canExplode;
+        canMove = _canMove;
+        moveSpeed = _moveSpeed;
     }
 
     void Update()
@@ -16,6 +27,37 @@ public class CrystalSkillController : MonoBehaviour
         crystalExistTimer -= Time.deltaTime;
 
         if (crystalExistTimer < 0)
+        {
+            FinishCrystal();
+        }
+
+        if (canGrow)
+        {
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(3, 3), growSpeed * Time.deltaTime);
+        }
+    }
+
+    private void AnimationExplodeEvent()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, cd.radius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+            {
+                hit.GetComponent<Enemy>().Damage();
+            }
+        }
+    }
+
+    public void FinishCrystal()
+    {
+        if (canExplode)
+        {
+            canGrow = true;
+            anim.SetTrigger("Explode");
+        }
+        else
         {
             SelfDestroy();
         }
