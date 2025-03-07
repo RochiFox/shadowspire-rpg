29be8c553f2985ea8cb4b5f38736822482b77f9c
Rochi <rochithefox@gmail.com>
2025-03-07 14:59:41 +0200

    Added: added stat script prototype

--- Shadowspire RPG/Assets/Scripts/CharacterStats.cs
+++ Shadowspire RPG/Assets/Scripts/CharacterStats.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public int damage;
-    public int maxHealth;
+    public Stat damage;
+    public Stat maxHealth;
 
     [SerializeField] private int currentHealth;
 
     void Start()
     {
-        currentHealth = maxHealth;
+        currentHealth = maxHealth.GetValue();
+
+        damage.AddModifier(4);
     }
 
     public void TakeDamage(int _damage)
