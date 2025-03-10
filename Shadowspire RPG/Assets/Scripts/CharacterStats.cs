1e46ba271406b4951347ea88af53fbf4440943e2
Rochi <rochithefox@gmail.com>
2025-03-10 11:57:11 +0200

    Added: added ailments effects and duration

--- Shadowspire RPG/Assets/Scripts/CharacterStats.cs
+++ Shadowspire RPG/Assets/Scripts/CharacterStats.cs
@@ -24,9 +24,16 @@ public class CharacterStats : MonoBehaviour
     public Stat iceDamage;
     public Stat lightingDamage;
 
-    public bool isIgnited;
-    public bool isChilled;
-    public bool isShocked;
+    public bool isIgnited; // does damage over time
+    public bool isChilled; // reduce armor by 20%
+    public bool isShocked; // reduce accuracy by 20%
+
+    private float ignitedTimer;
+    private float chilledTimer;
+    private float shockedTimer;
+    private float igniteDamageCooldown = 0.3f;
+    private float igniteDamageTimer;
+    private int igniteDamage;
 
 
     [SerializeField] private int currentHealth;
@@ -35,6 +42,43 @@ public class CharacterStats : MonoBehaviour
     {
         critPower.SetDefaultValue(150);
         currentHealth = maxHealth.GetValue();
+    }
+
+    protected virtual void Update()
+    {
+        ignitedTimer -= Time.deltaTime;
+        chilledTimer -= Time.deltaTime;
+        shockedTimer -= Time.deltaTime;
+        igniteDamageTimer -= Time.deltaTime;
+
+        if (ignitedTimer < 0)
+        {
+            isIgnited = false;
+        }
+
+        if (chilledTimer < 0)
+        {
+            isChilled = false;
+        }
+
+        if (shockedTimer < 0)
+        {
+            isShocked = false;
+        }
+
+        if (igniteDamageTimer < 0 && isIgnited)
+        {
+            Debug.Log("Take burn damage " + igniteDamage);
+            currentHealth -= igniteDamage;
+
+            if (currentHealth < 0)
+            {
+                Die();
+            }
+
+            igniteDamageTimer = igniteDamageCooldown;
+        }
+
     }
 
     public virtual void DoDamage(CharacterStats _targetStats)
@@ -101,6 +145,11 @@ public class CharacterStats : MonoBehaviour
             }
         }
 
+        if (canApplyIgnite)
+        {
+            _targetStats.SetupIgniteDamage(Mathf.RoundToInt(_fireDamage * 0.2f));
+        }
+
         _targetStats.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
     }
 
@@ -118,10 +167,29 @@ public class CharacterStats : MonoBehaviour
             return;
         }
 
-        isIgnited = _ignite;
+        if (_ignite)
+        {
+            isIgnited = _ignite;
+            ignitedTimer = 2;
+        }
+
+        if (_chill)
+        {
+            isChilled = _chill;
+            chilledTimer = 2;
+        }
+
+        if (_shock)
+        {
+            isShocked = _shock;
+            shockedTimer = 2;
+        }
+
         isChilled = _chill;
         isShocked = _shock;
     }
+
+    public void SetupIgniteDamage(int _damage) => igniteDamage = _damage;
 
     public virtual void TakeDamage(int _damage)
     {
@@ -142,7 +210,15 @@ public class CharacterStats : MonoBehaviour
 
     private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
     {
-        totalDamage -= _targetStats.armor.GetValue();
+        if (_targetStats.isChilled)
+        {
+            totalDamage -= Mathf.RoundToInt(_targetStats.armor.GetValue() * 0.8f);
+        }
+        else
+        {
+            totalDamage -= _targetStats.armor.GetValue();
+        }
+
         totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
         return totalDamage;
     }
@@ -150,6 +226,11 @@ public class CharacterStats : MonoBehaviour
     private bool TargetCanAvoidAttack(CharacterStats _targetStats)
     {
         int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+
+        if (isShocked)
+        {
+            totalEvasion += 20;
+        }
 
         if (Random.Range(0, 100) < totalEvasion)
         {
