5f617933cb930702f9243997669d374bd4941e8e
Rochi <rochithefox@gmail.com>
2025-03-12 11:51:18 +0200

    Added: added ailments visual effects

--- Shadowspire RPG/Assets/Scripts/CharacterStats.cs
+++ Shadowspire RPG/Assets/Scripts/CharacterStats.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    private EntityFX fx;
+
     [Header("Major stats")]
     public Stat strength; // 1 point increase damage by 1 and crit power by 1%
     public Stat agility; // 1 point increase evasion by 1% and crit chance by 1%
@@ -28,6 +30,8 @@ public class CharacterStats : MonoBehaviour
     public bool isChilled; // reduce armor by 20%
     public bool isShocked; // reduce accuracy by 20%
 
+    [SerializeField] private float ailmentsDuration = 4;
+
     private float ignitedTimer;
     private float chilledTimer;
     private float shockedTimer;
@@ -43,6 +47,8 @@ public class CharacterStats : MonoBehaviour
     {
         critPower.SetDefaultValue(150);
         currentHealth = GetMaxHealthValue();
+
+        fx = GetComponent<EntityFX>();
     }
 
     protected virtual void Update()
@@ -172,19 +178,25 @@ public class CharacterStats : MonoBehaviour
         if (_ignite)
         {
             isIgnited = _ignite;
-            ignitedTimer = 2;
+            ignitedTimer = ailmentsDuration;
+
+            fx.IgniteFxFor(ailmentsDuration);
         }
 
         if (_chill)
         {
             isChilled = _chill;
-            chilledTimer = 2;
+            chilledTimer = ailmentsDuration;
+
+            fx.ChillFxFor(ailmentsDuration);
         }
 
         if (_shock)
         {
             isShocked = _shock;
-            shockedTimer = 2;
+            shockedTimer = ailmentsDuration;
+
+            fx.ShockFxFor(ailmentsDuration);
         }
 
         isChilled = _chill;
--- Shadowspire RPG/Assets/Scripts/EntityFX.cs
+++ Shadowspire RPG/Assets/Scripts/EntityFX.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class EntityFX : MonoBehaviour
 {
@@ -11,6 +12,11 @@ public class EntityFX : MonoBehaviour
     [SerializeField] private Material hitMaterial;
     private Material originalMaterial;
 
+    [Header("Ailment colors")]
+    [SerializeField] private Color[] chillColor;
+    [SerializeField] private Color[] igniteColor;
+    [SerializeField] private Color[] shockColor;
+
     private void Start()
     {
         sr = GetComponentInChildren<SpriteRenderer>();
@@ -20,9 +26,13 @@ public class EntityFX : MonoBehaviour
     private IEnumerator FlashFX()
     {
         sr.material = hitMaterial;
+        Color currentColor = sr.color;
+
+        sr.color = Color.white;
 
         yield return new WaitForSeconds(flashDuration);
 
+        sr.color = currentColor;
         sr.material = originalMaterial;
     }
 
@@ -38,10 +48,64 @@ public class EntityFX : MonoBehaviour
         }
     }
 
-    private void CancelRedBlink()
+    private void CancelColorChange()
     {
         CancelInvoke();
 
         sr.color = Color.white;
+    }
+
+    public void IgniteFxFor(float _seconds)
+    {
+        InvokeRepeating("IgniteColorFX", 0, 0.3f);
+        Invoke("CancelColorChange", _seconds);
+    }
+
+    public void ChillFxFor(float _seconds)
+    {
+        InvokeRepeating("ChillColorFx", 0, 0.3f);
+        Invoke("CancelColorChange", _seconds);
+    }
+
+    public void ShockFxFor(float _seconds)
+    {
+        InvokeRepeating("ShockColorFx", 0, 0.3f);
+        Invoke("CancelColorChange", _seconds);
+    }
+
+    private void IgniteColorFX()
+    {
+        if (sr.color != igniteColor[0])
+        {
+            sr.color = igniteColor[0];
+        }
+        else
+        {
+            sr.color = igniteColor[1];
+        }
+    }
+
+    private void ChillColorFx()
+    {
+        if (sr.color != chillColor[0])
+        {
+            sr.color = chillColor[0];
+        }
+        else
+        {
+            sr.color = chillColor[1];
+        }
+    }
+
+    private void ShockColorFx()
+    {
+        if (sr.color != shockColor[0])
+        {
+            sr.color = shockColor[0];
+        }
+        else
+        {
+            sr.color = shockColor[1];
+        }
     }
 }
