3afe53093fcf13e3366404320ff00111441464d9
Rochi <rochithefox@gmail.com>
2025-03-13 14:59:58 +0200

    Added: added equipment items

--- Shadowspire RPG/Assets/Scripts/ItemData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Item Data", menuName = "Data/Item")]
-
-public class ItemData : ScriptableObject
-{
-    public string itemName;
-    public Sprite icon;
-}
