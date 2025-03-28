e8c4ebf5da04db5837891f6df55d8ce78ff9cd4a
Rochi <rochithefox@gmail.com>
2025-03-28 12:34:45 +0200

    Added: added switch UI with keyboard

--- Shadowspire RPG/Assets/Scripts/UI/UI.cs
+++ Shadowspire RPG/Assets/Scripts/UI/UI.cs
@@ -5,14 +5,42 @@ using UnityEngine;
 public class UI : MonoBehaviour
 {
     [SerializeField] private GameObject characterUI;
+    [SerializeField] private GameObject skillTreeUI;
+    [SerializeField] private GameObject craftUI;
+    [SerializeField] private GameObject optionsUI;
 
     public ItemTooltipUI itemTooltip;
     public StatTooltipUI statTooltip;
 
-    // Update is called once per frame
+    void Start()
+    {
+        SwitchTo(null);
+
+        itemTooltip.gameObject.SetActive(false);
+        statTooltip.gameObject.SetActive(false);
+    }
+
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SwitchWithKeyTo(characterUI);
+        }
 
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SwitchWithKeyTo(craftUI);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SwitchWithKeyTo(skillTreeUI);
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SwitchWithKeyTo(optionsUI);
+        }
     }
 
     public void SwitchTo(GameObject _menu)
@@ -26,5 +54,16 @@ public class UI : MonoBehaviour
         {
             _menu.SetActive(true);
         }
+    }
+
+    public void SwitchWithKeyTo(GameObject _menu)
+    {
+        if (_menu != null && _menu.activeSelf)
+        {
+            _menu.SetActive(false);
+            return;
+        }
+
+        SwitchTo(_menu);
     }
 }
