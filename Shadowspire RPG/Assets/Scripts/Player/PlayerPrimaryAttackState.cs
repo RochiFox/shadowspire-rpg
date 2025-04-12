e0c85422f8d6ada7953cd2c351773a2cc26466ad
Rochi <rochithefox@gmail.com>
2025-04-12 12:32:33 +0300

    Added: added audio manager and sounds

--- Shadowspire RPG/Assets/Scripts/Player/PlayerPrimaryAttackState.cs
+++ Shadowspire RPG/Assets/Scripts/Player/PlayerPrimaryAttackState.cs
@@ -17,6 +17,8 @@ public class PlayerPrimaryAttackState : PlayerState
     {
         base.Enter();
 
+        AudioManager.instance.PlaySFX(2); // Attack sfx
+
         xInput = 0;
 
         if (comboCounter > 2 || Time.time >= lastTimeAttacked + comboWindow)
