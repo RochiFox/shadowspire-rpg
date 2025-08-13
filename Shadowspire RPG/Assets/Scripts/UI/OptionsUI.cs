5b5dbe0d63952efec5048154481b69e189492430
Rochi <rochithefox@gmail.com>
2025-08-13 20:11:04 +0300

    Added: save and exit button functionality

--- Shadowspire RPG/Assets/Scripts/UI/OptionsUI.cs
+++ Shadowspire RPG/Assets/Scripts/UI/OptionsUI.cs
@@ -1,5 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsUI : MonoBehaviour
 {
+    [SerializeField] private Button saveAndExitButton;
+
+    private void Awake()
+    {
+        saveAndExitButton.onClick.AddListener(OnSaveAndExit);
+    }
+
+    private void OnSaveAndExit()
+    {
+        if (SaveManager.instance != null)
+        {
+            SaveManager.instance.SaveGame();
+        }
+
+        Application.Quit();
+    }
 }
